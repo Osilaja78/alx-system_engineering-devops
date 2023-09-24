7e9838c8e4c61b1fa54e66747d750c88eb15d739
Hameed Osilaja <osilajaabdulhameed@gmail.com>
2023-09-24 08:46:07 -0700

    fixed puppet lint erro

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -3,15 +3,15 @@ $ssh_config_file = '/etc/ssh/ssh_config'
 
 # Disable password authentication and configure identity file
 file_line { 'Turn off passwd auth':
+  ensure => present,
   path   => $ssh_config_file,
   line   => 'PasswordAuthentication no',
   match  => '^PasswordAuthentication',
-  ensure => present,
 }
 
 file_line { 'Declare identity file':
+  ensure => present,
   path   => $ssh_config_file,
   line   => 'IdentityFile ~/.ssh/school',
   match  => '^#?\s*IdentityFile',
-  ensure => present,
 }
