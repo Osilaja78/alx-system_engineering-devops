d5f37d9edeed93175fa172f353e077fb0e8df940
Hameed Osilaja <osilajaabdulhameed@gmail.com>
2023-09-26 14:31:38 -0700

    fixed task 5

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -4,18 +4,22 @@ package {'nginx':
   ensure => 'present',
 }
 
-exec {'install':
+exec {'update_and_install':
   command  => 'sudo apt-get update ; sudo apt-get -y install nginx',
   provider => shell,
 
 }
 
-exec {'Hello':
+exec {'Hello_world':
   command  => 'echo "Hello World!" | sudo tee /var/www/html/index.html',
   provider => shell,
 }
 
 exec {'sudo sed -i "s/listen 80 default_server;/listen 80 default_server;\\n\\tlocation \/redirect_me {\\n\\t\\treturn 301 https://www.youtube.com/watch?v=QH2-TGUlwu4\/;\\n\\t}/" /etc/nginx/sites-available/default':
+  provider => shell,
+}
+
+exec { 'sudo sed -i "/listen 80 default_server/a error_page 404 /404.html;\n \tlocation = /404.html {\n \t\troot /usr/share/nginx/html;\n\t\tinternal;\n\t}" /etc/nginx/sites-available/default':
   provider => shell,
 }
 
