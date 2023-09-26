874a779e39aede390bd0856908226041827d5152
Hameed Osilaja <osilajaabdulhameed@gmail.com>
2023-09-26 14:24:06 -0700

    modified code for task 5 m

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,37 +1,25 @@
-# This script configures a server with nginx.
+# This scritp configures nginx for a server.
 
-package { 'nginx':
-    provider => 'apt-get',
+package {'nginx':
+  ensure => 'present',
 }
 
-exec { 'allow_nginx':
-  command => 'ufw allow "Nginx Full"',
-  require => [Package['ufw'], Service['ufw']],
+exec {'install':
+  command  => 'sudo apt-get update ; sudo apt-get -y install nginx',
+  provider => shell,
+
 }
 
-exec { 'hello_world':
-  command => 'echo "Hello World!" | sudo tee /var/www/html/index.nginx-debian.html > /dev/null',
-  creates => '/var/www/html/index.nginx-debian.html',
-  require => [Package['nginx'], Service['nginx']],
+exec {'Hello':
+  command  => 'echo "Hello World!" | sudo tee /var/www/html/index.html',
+  provider => shell,
 }
 
-exec { 'error_file':
-  command => 'echo "Ceci n\'est pas une page" | sudo tee /usr/share/nginx/html/404.html',
-  creates => '/usr/share/nginx/html/404.html',
-  require => Package['nginx'],
+exec {'sudo sed -i "s/listen 80 default_server;/listen 80 default_server;\\n\\tlocation \/redirect_me {\\n\\t\\treturn 301 https://www.youtube.com/watch?v=QH2-TGUlwu4\/;\\n\\t}/" /etc/nginx/sites-available/default':
+  provider => shell,
 }
 
-exec { 'configure_redirection':
-  command => 'sudo sed -i "/listen 80 default_server/a location /redirect_me {\n  return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;\n}" /etc/nginx/sites-available/default',
-  require => Package['nginx'],
-}
-
-exec { 'configure_404':
-  command => 'sudo sed -i "/listen 80 default_server/a error_page 404 /404.html;\n \tlocation = /404.html {\n \t\troot /usr/share/nginx/html; \n\t\tinternal;\n    \t}" /etc/nginx/sites-available/default',
-  require => Package['nginx'],
-}
-
-service { 'nginx':
-  ensure  => running,
-  require => Package['nginx'],
+exec {'run':
+  command  => 'sudo service nginx restart',
+  provider => shell,
 }
