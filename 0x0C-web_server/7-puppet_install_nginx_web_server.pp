4490209e41dd0c8e40e9de20d2d917258782c262
Hameed Osilaja <osilajaabdulhameed@gmail.com>
2023-09-26 13:01:21 -0700

    added code for task 7

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,40 @@
+# This script configures an nginx server.
+
+package { 'nginx':
+	ensure => installed
+}
+
+file {'/usr/share/nginx/html/404.html':
+	content => 'Ceci n\'est pas une page',
+	mode => 0644
+}
+
+file { '/etc/nginx/sites-available/default':
+	content => "server {
+    	listen 80 default_server;
+
+    	root /usr/share/nginx/html;
+    	index index.html index.htm;
+
+    	location / {
+        	try_files \$uri \$uri/ =404;
+    	}
+
+    	error_page 404 /404.html;
+    	location = /404.html {
+		root /usr/share/nginx/html;
+        	internal;
+    	}
+
+    	location /redirect_me {
+        	return 301 https://www.example.com/; # Change this URL to your desired destination
+    	}
+	}
+	",
+	notify => Service['Nginx']
+}
+
+service {'nginx':
+	ensure => running,
+	enable => true
+}
