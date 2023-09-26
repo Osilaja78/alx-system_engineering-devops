a03b38268116b9efa091d297474d60a5780b9d16
Hameed Osilaja <osilajaabdulhameed@gmail.com>
2023-09-26 13:49:47 -0700

    codef or tsk 4

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,40 +1,37 @@
-# This script configures an nginx server.
+# This script configures a server with nginx.
 
 package { 'nginx':
-    ensure => installed
+    provider => 'apt-get',
 }
 
-file {'/usr/share/nginx/html/404.html':
-    content => 'Ceci n\'est pas une page',
-    mode    => '0644'
+exec { 'allow_nginx':
+  command => 'ufw allow "Nginx Full"',
+  require => [Package['ufw'], Service['ufw']],
 }
 
-file { '/etc/nginx/sites-available/default':
-    content => "server {
-    	listen 80 default_server;
-
-    	root /usr/share/nginx/html;
-    	index index.html index.htm;
-
-    	location / {
-        	try_files \$uri \$uri/ =404;
-    	}
-
-    	error_page 404 /404.html;
-    	location = /404.html {
-		root /usr/share/nginx/html;
-        	internal;
-    	}
-
-    	location /redirect_me {
-        	return 301 https://www.example.com/; # Change this URL to your desired destination
-    	}
-	}
-	",
-    notify  => Service['Nginx']
+exec { 'hello_world':
+  command => 'echo "Hello World!" | sudo tee /var/www/html/index.nginx-debian.html > /dev/null',
+  creates => '/var/www/html/index.nginx-debian.html',
+  require => [Package['nginx'], Service['nginx']],
 }
 
-service {'nginx':
-    ensure => running,
-    enable => true
+exec { '404_file':
+  command => 'echo "Ceci n'est pas une page" | sudo tee /usr/share/nginx/html/404.html',
+  creates => '/usr/share/nginx/html/404.html',
+  require => Package['nginx'],
+}
+
+exec { 'configure_redirection':
+  command => 'sudo sed -i "/listen 80 default_server/a location /redirect_me {\n  return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;\n}" /etc/nginx/sites-available/default',
+  require => Package['nginx'],
+}
+
+exec { 'configure_404':
+  command => 'sudo sed -i '/listen 80 default_server/a error_page 404 /404.html;\n \tlocation = /404.html {\n \t\troot /usr/share/nginx/html; \n\t\tinternal;\n    \t}' /etc/nginx/sites-available/default',
+  require => Package['nginx'],
+}
+
+service { 'nginx':
+  ensure => running,
+  require => Package['nginx'],
 }
